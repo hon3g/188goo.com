7ea8091dae015368e45bb0abcf787132a8e01b6e
hon3g <veston.zhao@gmail.com>
2021-11-12 18:45:50 -0500

    Add location related field choices

--- app/models.py
+++ app/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.base import Model
 from django.db.models.deletion import CASCADE, PROTECT
 from django.utils import timezone
 
+from choices import STATE_CHOICES, REGION_CHOICES
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=2, choices=REGION_CHOICES, default=None)
+
+    def __str__(self):
+        return self.name
+
 
 class State(models.Model):
-    name = models.CharField(max_length=25, unique=True)
+    name = models.CharField(max_length=2, choices=STATE_CHOICES)
 
     def __str__(self):
         return self.name
