1cc934b64033c29818e4287e424b2115959f98c1
hon3g <veston.zhao@gmail.com>
2021-11-15 16:54:22 -0500

    Rearrange city and state filtering logic

--- backend/app_api/views.py
+++ backend/app_api/views.py
@@ -10,11 +10,24 @@ class PostList(generics.ListAPIView):
     filter_backends = {DjangoFilterBackend}
     filterset_fields = {'id', 'category', 'slug'}
 
+    # # Filter by state name or city name
     def get_queryset(self):
-        current_state = self.request.query_params.get('state')
+        # City first than State
         current_city = self.request.query_params.get('city')
+        current_state = self.request.query_params.get('state')
 
-        # Filter by state name
+        if current_city is not None:
+            # Empty argument
+            if current_city == '':
+                pass
+            # Argument matches a city
+            elif len(City.objects.filter(name=current_city)) == 1:
+                city_id = City.objects.get(name=current_city).id
+                return Post.objects.filter(city=city_id)
+            # Not empty but doesn't match a city
+            else:
+                return Post.objects.filter(city=-1)
+
         if current_state is not None:
             # Empty argument
             if current_state == '':
@@ -28,18 +41,5 @@ class PostList(generics.ListAPIView):
             else:
                 return Post.objects.filter(city=-1)
 
-        # Filter by city name
-        if current_city is not None:
-            # Empty argument
-            if current_city == '':
-                pass
-            # Argument matches a city
-            elif len(City.objects.filter(name=current_city)) == 1:
-                city_id = City.objects.get(name=current_city).id
-                return Post.objects.filter(city=city_id)
-            # Not empty but doesn't match a city
-            else:
-                return Post.objects.filter(city=-1)
-
-        # If not passed in, return default
+        # If not passed-in or empty, return default
         return super().get_queryset()
