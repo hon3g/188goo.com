186f6395887b4fb03356f6040e83c09ddcc56989
hon3g <veston.zhao@gmail.com>
2021-11-24 22:41:09 -0500

    Save for the day

--- frontend/src/components/content/content.component.jsx
+++ frontend/src/components/content/content.component.jsx
@@ -6,20 +6,17 @@ import RadioGroup from '../radio-group/radio-group.component';
 import { Button } from 'antd';
 
 import 'antd/dist/antd.css';
-import './content.styles.scss'
-
+import './content.styles.scss';
 
 function Content() {
   return (
     <div>
       <SlideShow />
-      <div
-        style={{ backgroundColor: 'white', maxWidth: '61.8%', padding: '1.5rem' }}
-      >
+      <div className='list-content'>
         <CookieCrumb />
-        <div className='hon'>
-        <RadioGroup />
-        <Button type='primary'>+ 发布广告</Button>
+        <div className='location-and-post'>
+          <RadioGroup />
+          <Button type='primary'>+ 发布广告</Button>
         </div>
         <PostList />
       </div>
