184368f3490d6efa8606513acf3bf38bdae88bcd
hon3g <veston.zhao@gmail.com>
2022-10-02 12:55:57 -0400

    Fix ELB-HealthChecker 400

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'core.middleware.HealthCheckMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
